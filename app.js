2d6918604a42d5adcc4d919fa07a6769b36489ad
Jorge <hdez.jeduardo@gmail.com>
2018-09-23 23:53:40 -0500

    Update port

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const port = 3000
+const port = process.env.PORT || 8080;
+
 const app = express()
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
